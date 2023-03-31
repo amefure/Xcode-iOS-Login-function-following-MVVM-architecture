ef7cfb14002f51e78e34981480786b2f321e9087
ame <shachi10969@gmail.com>
2023-03-31 21:21:38 +0900

    Initial Commit

---
+++ TestAuth/TestAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAuthApp.swift
+//  TestAuth
+//
+//  Created by t&a on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct TestAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
